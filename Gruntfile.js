cf6f42d1833a77517729ab40632d76e6b902aa51
George Ashworth <georgeliuashworth@gmail.com>
2014-09-04 07:13:36 -0700

    set up grunt default task to serve site on localhost:8000

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,22 @@
 module.exports = function(grunt) {
-  grunt.loadNpmTasks('grunt-sass');
+
   
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    
+    'http-server': {
+      dev: {
+        root: '',
+        port: 8000,
+        host: '127.0.0.1',
+        showDir: true,
+        autoIndex: true,
+        defaultExt: 'html',
+        runInBackground: false
+      }
+    },
+    
+    
     sass: {
       options: {
         sourceMap: true
@@ -15,8 +29,9 @@ module.exports = function(grunt) {
     }
   });
 
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-http-server');
   
-  
-  grunt.registerTask('default', ['sass']);
+  grunt.registerTask('default', ['sass', 'http-server:dev']);
 
 };
